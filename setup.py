ef7b79a3ccfa7357b156f49ac063cab37fbe6d5f
Kitsunetic <shenhaichenhai@gmail.com>
2021-03-02 00:40:47 +0900

    version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme(file_name):
 
 setup(
     name="nb_black",
-    version="1.0.7",
+    version="1.0.8",
     description="A simple extension for Jupyter Notebook and Jupyter Lab to beautify Python code automatically using Black.",
     long_description=readme(file_name="README.md"),
     long_description_content_type="text/markdown",
